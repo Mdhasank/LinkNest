db9af653ddb2c52d3a1657dfbc5d3fa5576a5809
Mohd Hasan Khan <101917422+Mdhasank@users.noreply.github.com>
2025-10-12 15:54:01 +0530

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -2,6 +2,12 @@
 
 cd /home/ec2-user/my-python-app || exit 1
 
+# Install dependencies
+echo "Installing Python dependencies..."
+pip3 install -r requirements.txt
+
+# Start the app
+echo "Starting the application..."
 nohup python3 app.py > app.log 2>&1 &
 
 echo "Application started"
